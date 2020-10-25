c66a47bc3ce5726621169d0a8ec7a3b3c855e10a
bentaylor <ben10taylor10@hotmail.com>
2020-10-25 17:26:48 +0000

    removed banner padding

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDuaYCBF9h7IlopXRzuwSUsn6JBeDNmliE",
+    authDomain: "artlogic-f6553.firebaseapp.com",
+    databaseURL: "https://artlogic-f6553.firebaseio.com",
+    projectId: "artlogic-f6553",
+    storageBucket: "artlogic-f6553.appspot.com",
+    messagingSenderId: "946801095313",
+    appId: "1:946801095313:web:165a760011fd44604b4530",
+    measurementId: "G-8GDXKQ3M3K"
+  };
+
+  firebase.initializeApp(firebaseConfig);
